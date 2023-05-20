dc2e5d87554e5c43c9ae5f015b094e145ee0b4ca
Erik Rydberg <erydberg@gmail.com>
2023-05-20 08:46:39 +0200

    försök med 3.10 spring boot

--- src/main/java/se/rydberg/handla/Handla2Application.java
+++ src/main/java/se/rydberg/handla/Handla2Application.java
@@ -4,7 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
 
 @SpringBootApplication
 public class Handla2Application {
